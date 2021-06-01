7dae9a4eea4abaa8dfe8b9ed766ee318bdb439ab
odys-hub <85128793+odys-hub@users.noreply.github.com>
2021-06-01 12:50:55 +0200

    fix

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ app.use(express.static(path.join(__dirname, "client")));
 app.use(bodyParser.json());
 
 const publicVapidKey =
-  "BJthRQ5myDgc7OSXzPCMftGw-n16F7zQBEN7EUD6XxcfTTvrLGWSIG7y_JxiWtVlCFua0S8MTB5rPziBqNx1qIo";
-const privateVapidKey = "3KzvKasA2SoCxsp0iIG_o9B0Ozvl1XDwI63JRKNIWBM";
+  "BEMaNnyNdrY496lsixlaHWvAFqh-RzZtqKLRtzAJ_WpVxBpHh8bsw8fiJ9Zl3wC0xnx9OXUhKYxbRM26xFOAIvU";
+const privateVapidKey = "n4e_moQ0IvZ8nrgw9BqBNlJIdBwY5hdk3isHVblEs9s";
 
 webpush.setVapidDetails(
   "mailto:test@test.com",
